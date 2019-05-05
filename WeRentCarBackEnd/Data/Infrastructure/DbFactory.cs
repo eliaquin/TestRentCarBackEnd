f5a07801b52eb9e616a9177c5dbe30f7a13205d8
Eliaquín Encarnación <eliaquin@outlook.com>
2019-05-05 00:40:46 -0400

    Added factory and repository

---
+++ WeRentCarBackEnd/Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace WeRentCarBackEnd.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        ApplicationDbContext Init();
+    }
+
+    public class DbFactory : IDbFactory
+    {
+        private ApplicationDbContext _dbContext;
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public DbFactory(IConfiguration configuration)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            var options = optionsBuilder.Options;
+            _options = options;
+        }
+
+        public ApplicationDbContext Init()
+        {
+            return _dbContext ?? (_dbContext = new ApplicationDbContext(_options));
+        }
+
+        public void Dispose()
+        {
+            _dbContext?.Dispose();
+        }
+    }
+}
---
+++ WeRentCarBackEnd/Data/Infrastructure/Repository.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WeRentCarBackEnd.Data.Infrastructure
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(Expression<Func<T, bool>> where);
+        T GetById(int id);
+        T Get(Expression<Func<T, bool>> where);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetMany(Expression<Func<T, bool>> where);
+        bool Any();
+        bool Any(Expression<Func<T, bool>> where);
+        int SaveChanges();
+    }
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _dbSet;
+        public readonly ApplicationDbContext Context;
+
+        public Repository(IDbFactory factory)
+        {
+            var context = factory.Init();
+            _dbSet = context.Set<T>();
+            Context = context;
+        }
+
+        public virtual void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual void Delete(Expression<Func<T, bool>> where)
+        {
+            var objects = _dbSet.Where(where).AsEnumerable();
+            foreach (var obj in objects)
+                _dbSet.Remove(obj);
+        }
+
+        public virtual T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual IEnumerable<T> GetMany(Expression<Func<T, bool>> where)
+        {
+            return _dbSet.Where(where).ToList();
+        }
+
+        public bool Any()
+        {
+            return _dbSet.Any();
+        }
+
+        public bool Any(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Any(predicate);
+        }
+
+        public T Get(Expression<Func<T, bool>> where)
+        {
+            return _dbSet.Where(where).FirstOrDefault();
+        }
+
+        public int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+    }
+
+}
