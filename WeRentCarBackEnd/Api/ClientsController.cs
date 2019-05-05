135653e243e1d634c813dd6eefabba08fdb3e8d1
Eliaquín Encarnación <eliaquin@outlook.com>
2019-05-05 07:24:37 -0400

    Added controllers

---
+++ WeRentCarBackEnd/Api/ClientsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WeRentCarBackEnd.Models;
+using WeRentCarBackEnd.Services;
+
+namespace WeRentCarBackEnd.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IClientService _clientService;
+
+        public ClientsController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        [HttpGet]
+        [Route("getallclients")]
+        public IEnumerable<Client> GetAllClients()
+        {
+            return _clientService.GetAll();
+        }
+    }
+}
---
+++ WeRentCarBackEnd/Api/VehiclesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WeRentCarBackEnd.Models;
+using WeRentCarBackEnd.Services;
+
+namespace WeRentCarBackEnd.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IVehicleService _vehicleService;
+
+        public VehiclesController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        public IEnumerable<Vehicle> GetAllVehicles()
+        {
+            return _vehicleService.GetAll();
+        }
+    }
+}
